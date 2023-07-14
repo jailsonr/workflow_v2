fa2eeb55f3ce4e211fb1064b5d3f07284df68d39
scabrero <scabrero@SAN-4X8P493.usersad.everis.int>
2023-07-14 13:09:58 -0400

    Se agregan querys a QueryEnum y parametros a PropertiesUtyil

--- src/cl/security/database/utils/QueryEnum.java
+++ src/cl/security/database/utils/QueryEnum.java
@@ -5,7 +5,18 @@ import cl.security.utils.PropertiesUtil;
 public enum QueryEnum {
 
 	VERIFY_MESSAGES("SELECT * FROM " + PropertiesUtil.MESSAGES),
-	GET_DEAL_LIST("SELECT * FROM Kustom..WKF_DealsList WHERE Status = 'P' AND Retries = 0");
+	GET_DEAL_LIST("SELECT * FROM " + PropertiesUtil.DEAL + " WHERE Status = 'P' AND Retries = 0"),
+	UPDATE_FLAGS_DEALS("{call Kustom.." + PropertiesUtil.FLAGS + "(?,?,?,?,?,?)}"),
+	MESSAGES_IN_PROGRESS_DELETE("{call Kustom.." + PropertiesUtil.REGISTRYDELETE + "(?,?)}"),
+	IMPORT_FILE("{call Kustom.."+ PropertiesUtil.KISFIELDS + "(?,?,?)}"),
+	GET_KIS_DEAL_ID("{call Kustom.."+ PropertiesUtil.GETKISDEAL + "(?,?,?)}"),
+	MLS_STATUS_GET("{call Kustom.." + PropertiesUtil.MLSGET + "(?,?,?)}"),
+	EXCEEDED_DEALS_ACCEPTANCE_INSERT("{call Kustom.." + PropertiesUtil.EDAI + "(?,?,?)}"),
+	EXCEEDED_DEALS_INSERT("{call Kustom.." + PropertiesUtil.EDI + "(?,?,?,?,?)}"),
+	MLS_DEAL_RESULT_GET("{call Kustom.." + PropertiesUtil.MLSRESULT + "(?,?,?,?)}"),
+	KGR_STATUS_GET("{call " + PropertiesUtil.KGRGET + "(?,?,?,?,?,?,?,?)}"),
+	DEAL_LIST_UPDATE("{call " + PropertiesUtil.DEALLISTUPDATE + "(?,?,?)}")
+	;
 
 	public final String query;
 
